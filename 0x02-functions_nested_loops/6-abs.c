a61d895b745cf159bebd544469682d595acf2bc6
Jean Damascene Benimana <benidama02@gmail.com>
2023-03-22 13:32:36 -0700

    computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function tha computes the absolute value of an integer
+ * @u: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int u)
+{
+if (u >= 0)
+{
+return (u);
+}
+return (-u);
+}
+
