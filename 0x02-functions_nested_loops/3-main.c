897f9dadf54a98a6530a2f5c24635d564db6afec
Jean Damascene Benimana <benidama02@gmail.com>
2023-03-22 10:41:02 -0700

    a function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int p;
+p = _islower('H');
+_putchar(p + '0');
+p = _islower('o');
+_putchar(p + '0');
+p = _islower(108);
+_putchar(p + '0');
+_putchar('\n');
+return (0);
+}
