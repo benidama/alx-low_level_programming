ec97fc47ae43299b8d53a54a2d48306f44235382
Jean Damascene Benimana <benidama02@gmail.com>
2023-03-22 12:30:33 -0700

    function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,18 +14,14 @@ if (u > 0)
 _putchar(43);
 return (1);
 }
-else if (u == 0)
-{
-_putchar(48);
-return (0);
-}
 else if (u < 0)
-{
-_putchar('-');
-}
-else
 {
 _putchar(45);
 return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
 }
 }
