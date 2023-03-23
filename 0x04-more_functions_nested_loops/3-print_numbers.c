4fb6222dc2ddba1f0ce48143981223ecd35f576b
Jean Damascene Benimana <benidama02@gmail.com>
2023-03-23 13:31:27 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+char u;
+for (u = '0'; u <= '9'; u++)
+{
+_putchar(u);
+}
+_putchar('\n');
+}
