bbb7b9584992aee9cefe9f9091d04448a443de95
Jean Damascene Benimana <benidama02@gmail.com>
2023-04-05 05:05:19 -0700

    Program that prints string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strlen_recursion - Calculate the length of a string
+  * @s: input string
+  *
+  * Return: string length
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
