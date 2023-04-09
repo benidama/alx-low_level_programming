b28a50458a21bf608e8e1bf31a7142910b748a96
Jean Damascene Benimana <benidama02@gmail.com>
2023-04-09 10:56:52 -0700

    Program prints a line with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - Prints a line with function 
- * Return:0 (Success)
+ * main - Prints a line with puts function
+ * Return: 0 on Success
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
