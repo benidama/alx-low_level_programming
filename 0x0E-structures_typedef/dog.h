da16cf92ae2b15c0a227646b21cf66ab1b893298
Jean Damascene Benimana <benidama02@gmail.com>
2023-05-05 10:23:29 -0700

    The prototype of Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
   * struct dog - Dog attributes
@@ -18,14 +19,10 @@ struct dog
 /**
  * dog_t - Typedef for dog structure
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif
